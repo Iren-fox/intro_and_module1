d8e10b7a738986a083ac3d28123ba5bb4e3140b4
Iren-fox <fox.iren.fox@yandex.ru>
2024-07-16 15:39:06 +0300

    Add files via upload

---
+++ homework4.py
@@ -0,0 +1,9 @@
+my_string=input("Введите слово или фразу: ")
+print(len(my_string))
+print(my_string.upper())
+print(my_string.lower())
+print(my_string.replace(' ',''))
+first_symbol=my_string[0]
+print(first_symbol)
+last_symbol=my_string[-1]
+print(last_symbol)
